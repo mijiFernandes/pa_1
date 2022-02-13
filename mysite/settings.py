60461a8f5af02ef70fe661d85e6148faeb27e05f
mijiFernandes <lkm366859@gmail.com>
2022-02-13 11:35:18 +0900

    Added files and changes required for deployment to heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -152,4 +152,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 
-DEFAULT_FILE_STORAGE = 'mysite.storage.S3MediaStorages'
+DEFAULT_FILE_STORAGE = 'mysite.storage.S3MediaStorage'
