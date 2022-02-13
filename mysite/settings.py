6bfe8e468a3806ba157794cc7c93cde155a32fea
mijiFernandes <lkm366859@gmail.com>
2022-02-13 15:26:03 +0900

    Added files and changes required for deployment to heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -153,3 +153,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = '/'
 
 DEFAULT_FILE_STORAGE = 'mysite.storage.S3MediaStorage'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
