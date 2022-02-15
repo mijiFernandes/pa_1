bf5c3cf906a2ce3786ac3afe300b4380b27528e0
mijiFernandes <lkm366859@gmail.com>
2022-02-15 18:27:33 +0900

    Added files and changes required for deployment to heroku

--- users/models.py
+++ users/models.py
@@ -17,7 +17,7 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super().save()
 
-        img = Image.read(self.avatar.name)
+        img = Image.open('media/' + self.avatar.name)
 
         if img.height > 100 or img.width > 100:
             new_img = (100, 100)
