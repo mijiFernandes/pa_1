1fd640538685e1d1900d6743bb57d376db0b1e75
mijiFernandes <lkm366859@gmail.com>
2022-02-15 16:41:47 +0900

    Added files and changes required for deployment to heroku

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    avatar = models.ImageField(default='img/default.png', upload_to='profile_images/%Y/%m')
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images/%Y/%m')
     bio = models.TextField()
 
     def __str__(self):
