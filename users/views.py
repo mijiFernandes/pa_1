21f47ad705103a86be539fa9832ba888b58c723f
mijiFernandes <lkm366859@gmail.com>
2022-02-15 20:41:11 +0900

    Added files and changes required for deployment to heroku

--- users/views.py
+++ users/views.py
@@ -87,9 +87,9 @@ def profile(request):
 
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
-            # profile_form.save()
+            profile_form.save()
             messages.success(request, 'Your profile is updated successfully')
-            return reverse_lazy('home')
+            return redirect(to='users-profile')
     else:
         user_form = UpdateUserForm(instance=request.user)
         profile_form = UpdateProfileForm(instance=request.user.profile)
