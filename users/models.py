394ef649beb79c011f768f04a978482097c63c60
mijiFernandes <lkm366859@gmail.com>
2022-02-15 20:24:41 +0900

    Added files and changes required for deployment to heroku

--- users/models.py
+++ users/models.py
@@ -13,11 +13,11 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-    # resizing images
-    def save(self, *args, **kwargs):
-        super().save()
-        urllib.request.urlretrieve(self.avatar.url, "image.png")
-        img = Image.open("image.png")
-        if img.height > 100 or img.width > 100:
-            new_img = (100, 100)
-            img.thumbnail(new_img)
+    # # resizing images
+    # def save(self, *args, **kwargs):
+    #     super().save()
+    #     urllib.request.urlretrieve(self.avatar.url, "image.png")
+    #     img = Image.open("image.png")
+    #     if img.height > 100 or img.width > 100:
+    #         new_img = (100, 100)
+    #         img.thumbnail(new_img)
