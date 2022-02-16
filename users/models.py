2e4de05fa85a1ee440700be09205eb02e01ece99
mijiFernandes <lkm366859@gmail.com>
2022-02-16 23:58:31 +0900

    Added files and changes required for deployment to heroku

--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
 
     avatar = models.ImageField(default="default.jpg", upload_to='profile_images/%Y/%m')
     bio = models.TextField()
-    point = models.IntegerField(default=0)
+    point = 0
 
     def __str__(self):
         return self.user.username
